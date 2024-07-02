0ed6806a5e9b900142fae8ca373debf4ca5f7772
Anzlc <107500985+Anzlc@users.noreply.github.com>
2024-07-02 20:05:17 +0200

    novi  testi

--- src/test.py
+++ src/test.py
@@ -1,7 +1,9 @@
 import time as t
 
-ime = input("Vnesi natisni svoje ime: ")
+ime = input("natisni svoje ime: ")
+svet = "svet"
 print(f"Živo, {ime}")
+print(svet)
 
 for i in range(10, 0, -1):
     print(f"{i}!")
