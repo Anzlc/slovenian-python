bd8f8c69dcb01fb4c761a7dfa6e77d2bc75c45eb
Anzlc <107500985+Anzlc@users.noreply.github.com>
2024-07-03 15:44:01 +0200

    CLI, som improvements

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,85 @@
-use std::{ collections::HashMap, fs::read_to_string, fs::write };
+use std::fs::read_to_string;
 mod transpiler;
 mod file;
 use file::write_and_empty;
 use transpiler::{ tokenize, transpile };
-use std::process::Command;
+extern crate getopts;
+use getopts::Options;
+use std::env;
+use std::process::{ exit, Command };
+
 fn main() {
-    let mut keywords: HashMap<String, String> = HashMap::new();
-    keywords.insert("definiraj".to_string(), "def".to_string());
-    keywords.insert("za".to_string(), "for".to_string());
-    keywords.insert("v".to_string(), "in".to_string());
-    keywords.insert("dokler".to_string(), "while".to_string());
-    keywords.insert("vključi".to_string(), "import".to_string());
-    keywords.insert("iz".to_string(), "from".to_string());
-    keywords.insert("vrni".to_string(), "return".to_string());
-    keywords.insert("natisni".to_string(), "print".to_string());
-    keywords.insert("obseg".to_string(), "range".to_string());
-    keywords.insert("kot".to_string(), "as".to_string());
-    keywords.insert("vnos".to_string(), "input".to_string());
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
 
-    let program = read_to_string("./src/test.spy").unwrap();
-    let tokens = tokenize(&program, &keywords);
-    println!("{:#?}", tokens);
-    let new_program = transpile(&tokens, &keywords);
-    write_and_empty("./src/test.py", &new_program).unwrap();
+    opts.optopt("t", "transpile", "Create a python file from .spy", "PATH");
+    opts.optopt("r", "run", "Run the .spy program", "PATH");
+    opts.optflag("h", "help", "Get help (output this screen)");
 
-    println!("{}", new_program);
+    let matches = opts.parse(&args[1..]).unwrap_or_else(|f| {
+        println!("An error occured: {f}");
+        exit(1);
+    });
+
+    if matches.opt_present("h") {
+        get_help(opts);
+        return;
+    }
+
+    if let Some(path) = matches.opt_str("r") {
+        run(&path);
+    } else if let Some(path) = matches.opt_str("t") {
+        transpile_and_write(&path);
+        println!("Done");
+    } else if args.len() == 2 {
+        run(&args[1]);
+    }
+}
+
+fn run(path: &str) {
+    match read_to_string(path) {
+        Ok(program) => {
+            let tokens = tokenize(&program);
+            //println!("{:#?}", tokens);
+            let new_program = transpile(&tokens);
+            match write_and_empty(&spy_to_py(path), &new_program) {
+                Ok(_) => {
+                    //Run with python
+                    match Command::new("python").arg(&spy_to_py(path)).status() {
+                        Ok(_) => {}
+                        Err(e) => println!("Error: {e}"),
+                    }
+                }
+                Err(e) => println!("Error while writing file: {e}"),
+            }
+        }
+        Err(e) => println!("Error while reading file: {e}"),
+    }
+}
+
+fn transpile_and_write(path: &str) {
+    match read_to_string(path) {
+        Ok(program) => {
+            let tokens = tokenize(&program);
+            //println!("{:#?}", tokens);
+            let new_program = transpile(&tokens);
+            match write_and_empty(&spy_to_py(path), &new_program) {
+                Ok(_) => {}
+                Err(e) => println!("Error while writing file: {e}"),
+            }
+        }
+        Err(e) => println!("Error while reading file: {e}"),
+    }
+}
+
+fn spy_to_py(path: &str) -> String {
+    path.split(".")
+        .map(|s| if s == "spy" { "py".to_string() } else { s.to_string() })
+        .collect::<Vec<String>>()
+        .join(".")
+}
+
+fn get_help(opts: Options) {
+    let brief = "Usage: piton FILE [options]";
+    print!("{}", opts.usage(&brief));
 }
--- src/transpiler.rs
+++ src/transpiler.rs
@@ -1,4 +1,23 @@
-use std::{ collections::HashMap, hash::Hash, sync::Arc };
+use std::collections::HashMap;
+use phf::phf_map;
+
+static KEYWORDS: phf::Map<
+    &'static str,
+    &'static str
+> = phf_map! {
+    "definiraj" => "def",
+    "za" => "for",
+    "v" => "in",
+    "dokler" => "while",
+    "vključi" => "import",
+    "iz" => "from",
+    "vrni" => "return",
+    "natisni" => "print",
+    "obseg" => "range",
+    "kot" => "as",
+    "vnos" => "input",
+    "če" => "if",
+};
 
 #[derive(Debug)]
 pub struct Token {
@@ -10,12 +29,15 @@ impl Token {
     fn new(value: String, is_keyword: bool) -> Token {
         Token { value, is_keyword }
     }
-
-    fn get_new_value(&self, keywords: &HashMap<String, String>) -> String {
+    fn from_string(value: &String) -> Token {
+        let tmp = value.clone();
+        Token::new(value.to_string(), KEYWORDS.contains_key(tmp.as_str()))
+    }
+    fn get_new_value(&self) -> String {
         if !self.is_keyword {
             return self.value.to_string();
         }
-        match keywords.get(self.value.as_str()) {
+        match KEYWORDS.get(self.value.as_str()) {
             Some(x) => {
                 return x.to_string();
             }
@@ -26,68 +48,57 @@ impl Token {
     }
 }
 
-pub fn tokenize(program: &String, keywords: &HashMap<String, String>) -> Vec<Token> {
+pub fn tokenize(program: &String) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
-    // for c in program.chars() {
-    //     if " ():\"'\n\r".contains(c) {
-    //         tokens.push(
-    //             Token::new(
-    //                 buf.clone(),
-    //                 keywords.keys().any(|x| x.to_string() == buf)
-    //             )
-    //         );
-    //         buf.clear();
-    //         tokens.push(Token::new(c.to_string(), false));
-    //         continue;
-    //     }
-    //     buf.push(c);
-    // }
-    let mut buf: String = String::new();
-    let mut pairs: HashMap<String, String> = HashMap::new();
-    pairs.insert("(".to_string(), ")".to_string());
-    pairs.insert("\"".to_string(), "\"".to_string());
-    pairs.insert("'".to_string(), "'".to_string());
-    pairs.insert("[".to_string(), "]".to_string());
 
-    let single_tokens = ": \n\r.=";
+    let mut buf: String = String::new();
+    let mut pairs: HashMap<&str, &str> = HashMap::new();
+    pairs.insert("\"", "\"");
+    pairs.insert("'", "'");
+
+    let single_tokens = ": \n\r.=()[]";
 
     let mut i: usize = 0;
-    println!();
     while i < program.chars().count() {
-        let ch = program.chars().nth(i).unwrap();
+        let tmp = program.chars().nth(i).unwrap().to_string();
+        let ch = tmp.as_str();
 
-        if pairs.contains_key(&ch.to_string()) {
-            tokens.push(Token::new(buf.to_string(), keywords.contains_key(&buf)));
-            buf.clear();
-            let mut j = i;
-            while
-                j < program.len() &&
-                &program.chars().nth(j).unwrap().to_string() != pairs.get(&ch.to_string()).unwrap()
-            {
-                buf.push(program.chars().nth(j).unwrap());
+        if pairs.contains_key(ch) {
+            if buf.len() > 0 {
+                tokens.push(Token::from_string(&buf.to_string()));
+                buf.clear();
+            }
+            let pair = pairs.get(ch).unwrap(); // Can safely unwrap because we check it before
+            buf.push_str(ch);
+            let mut j = i + 1;
+
+            while j < program.len() && &program.chars().nth(j).unwrap().to_string() != pair {
+                buf.push_str(&program.chars().nth(j).unwrap().to_string());
                 j += 1;
             }
-            buf.push_str(pairs.get(&ch.to_string()).unwrap().as_str());
-            tokens.push(Token::new(buf.to_string(), keywords.contains_key(&buf)));
+            buf.push_str(pairs.get(ch).unwrap());
+            tokens.push(Token::from_string(&buf.to_string()));
             buf.clear();
             i = j + 1;
 
             continue;
         } else if single_tokens.contains(ch) {
-            tokens.push(Token::new(buf.to_string(), keywords.contains_key(&buf)));
-            buf.clear();
-            tokens.push(Token::new(ch.to_string(), keywords.contains_key(&ch.to_string())));
+            if buf.len() > 0 {
+                tokens.push(Token::from_string(&buf.to_string()));
+                buf.clear();
+            }
+            tokens.push(Token::from_string(&ch.to_string()));
         } else {
-            buf.push(ch);
+            buf.push_str(ch);
         }
         i += 1;
     }
     return tokens;
 }
-pub fn transpile(tokens: &Vec<Token>, keywords: &HashMap<String, String>) -> String {
-    let mut new_tokens = tokens
+pub fn transpile(tokens: &Vec<Token>) -> String {
+    let new_tokens = tokens
         .iter()
-        .map(|token| token.get_new_value(&keywords))
+        .map(|token| token.get_new_value())
         .collect::<Vec<String>>()
         .join("");
 
